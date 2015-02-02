086ed4a237de9d7d5f125a4af1b5bf1d8bee1f96
leejaedus <leejaeduss@gmail.com>
2015-02-02 10:33:41 +0900

    convert html to jade

--- app/controllers/index.py
+++ app/controllers/index.py
@@ -1,10 +1,15 @@
 # -*-coding:utf-8-*-
-from flask import render_template
-
+from flask import render_template, send_from_directory
 from . import index_blueprint
+from app import app
 
 
 @index_blueprint.route('/')
 @index_blueprint.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.jade')
+
+
+@app.route('/static/<path>')
+def get_static(path):
+    return send_from_directory('static', path)
